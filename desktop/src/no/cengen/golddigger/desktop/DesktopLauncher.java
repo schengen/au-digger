34dd924193668b937b1dd83bddf439baad995018
Christian Engen <christian.engen@avanade.com>
2016-04-29 17:52:51 +0200

    first commit

---
+++ desktop/src/no/cengen/golddigger/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.cengen.golddigger.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.cengen.golddigger.GoldDigger;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GoldDigger(), config);
+	}
+}
